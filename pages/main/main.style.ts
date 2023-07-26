9edcf122b34c6af6d8b3e4bdd7ef08a40765599b
SuJin <kimsujin001127@gmail.com>
2023-07-26 16:04:29 +0900

    feat: 스타일 파일 생성

---
+++ pages/main/main.style.ts
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 375px;
+  background-color: white;
+`;
+
+export const Header = styled.div``;
+
+export const MenuBox = styled.div``;
+
+export const LogoBox = styled.div``;
+
+export const TopBox = styled.div``;
+
+export const TopTitleBox = styled.div``;
+
+export const TopDescriptionBox = styled.div``;
+
+export const SearchBox = styled.div``;
+
+export const Search = styled.div``;
+
+export const ListBox = styled.div``;
+export const PicBox = styled.div``;
+
+export const RightBox = styled.div``;
+export const ListTopBox = styled.div``;
+export const ListBottomBox = styled.div``;
+
+export const ListDateBox = styled.div``;
+
+export const ListTagBox = styled.div``;
+
+export const Tag = styled.div``;
