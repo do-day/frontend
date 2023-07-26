483a582db89bc05a0a3410ec8545fb23a8c6feb8
SuJin <kimsujin001127@gmail.com>
2023-07-26 16:04:48 +0900

    feat: 기본 화면 구성

---
+++ pages/main/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+import * as styles from './main.style';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <styles.Container>
+      <styles.Header>
+        <styles.MenuBox></styles.MenuBox>
+        <styles.LogoBox>DO DAY</styles.LogoBox>
+      </styles.Header>
+      <styles.TopBox>
+        <styles.TopTitleBox>신고목록</styles.TopTitleBox>
+        <styles.TopDescriptionBox>
+          해결하고 싶은 신고를 선택해 주세요.
+        </styles.TopDescriptionBox>
+      </styles.TopBox>
+      <styles.SearchBox>
+        <styles.Search>
+          <input placeholder="Search"></input>
+        </styles.Search>
+      </styles.SearchBox>
+      <styles.ListBox>
+        <styles.PicBox></styles.PicBox>
+        <styles.RightBox></styles.RightBox>
+        <styles.ListTopBox>서울특별시 동작구 노량진로 10</styles.ListTopBox>
+        <styles.ListBottomBox>
+          <styles.ListDateBox>최초 신고일: 2023.07.01 18:30</styles.ListDateBox>
+          <styles.ListTagBox>
+            <styles.Tag>미해결</styles.Tag>
+          </styles.ListTagBox>
+        </styles.ListBottomBox>
+      </styles.ListBox>
+    </styles.Container>
+  );
+}
