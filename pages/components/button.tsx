73f655a461c4fe8be7149647a2ec00a03fd3c688
hhkim0729 <hhkim0729@gmail.com>
2023-07-27 18:36:33 +0900

    feat: 하단 모달 추가

--- pages/components/button.tsx
+++ pages/components/button.tsx
@@ -2,23 +2,30 @@ import styled from '@emotion/styled';
 
 interface Props {
   children: React.ReactNode;
-  type: 'button' | 'submit';
+  type?: 'button' | 'submit';
   primary?: boolean;
+  rounded?: boolean;
 }
 
-export default function Button({ children, type, primary = true }: Props) {
+export default function Button({
+  children,
+  type = 'button',
+  primary = true,
+  rounded = false,
+}: Props) {
   return (
-    <StyledButton primary={primary} type={type}>
+    <StyledButton type={type} primary={primary} rounded={rounded}>
       {children}
     </StyledButton>
   );
 }
 
-const StyledButton = styled.button<{ primary: boolean }>`
+const StyledButton = styled.button<{ primary: boolean; rounded: boolean }>`
   width: 100%;
   height: 3rem;
   color: var(--color-white);
   background-color: ${(props) =>
     props.primary ? 'var(--color-main)' : 'var(--color-gray)'};
-  border-radius: var(--border-radius);
+  border-radius: ${(props) =>
+    props.rounded ? '2rem' : 'var(--border-radius)'};
 `;
--- pages/report/new.tsx
+++ pages/report/new.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import styled from '@emotion/styled';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
 import HeaderBack from '@/pages/components/header';
 import Button from '@/pages/components/button';
+import ModalBottom from '@/pages/components/modalBottom';
 
 export default function ReportNew() {
+  const [showPhotoModal, setShowPhotoModal] = useState(false);
+
   return (
     <>
       <HeaderBack title="신고하기" />
@@ -19,10 +24,13 @@ export default function ReportNew() {
             <StyledDiv>
               <h2>사진 첨부</h2>
               <ImageDiv>
-                <PhotoDiv>
+                <PhotoDiv
+                  size={'7.5rem'}
+                  onClick={() => setShowPhotoModal(true)}
+                >
                   <FontAwesomeIcon icon={faPlus} />
                 </PhotoDiv>
-                <PhotoDiv>
+                <PhotoDiv size={'7.5rem'}>
                   <FontAwesomeIcon icon={faPlus} />
                 </PhotoDiv>
               </ImageDiv>
@@ -38,6 +46,41 @@ export default function ReportNew() {
           <Button type="submit">신고하기</Button>
         </StyledForm>
       </main>
+
+      {showPhotoModal && (
+        <ModalBottom onClose={() => setShowPhotoModal(false)}>
+          <Modal>
+            <h2>이런 사진을 올려주세요</h2>
+            <ImageDiv>
+              <div>
+                <h3>빗물받이</h3>
+                <PhotoDiv size="15vh">
+                  <Image
+                    src="/example1.png"
+                    alt="빗물받이"
+                    layout="fill"
+                    objectFit="contain"
+                  />
+                </PhotoDiv>
+              </div>
+              <div>
+                <h3>주변 건물</h3>
+                <PhotoDiv size="15vh">
+                  <Image
+                    src="/example2.png"
+                    alt="주변 건물"
+                    layout="fill"
+                    objectFit="contain"
+                  />
+                </PhotoDiv>
+              </div>
+            </ImageDiv>
+            <ButtonWrapper>
+              <Button rounded>사진 보관함</Button>
+            </ButtonWrapper>
+          </Modal>
+        </ModalBottom>
+      )}
     </>
   );
 }
@@ -85,9 +128,11 @@ const ImageDiv = styled.div`
   gap: 1rem;
 `;
 
-const PhotoDiv = styled.div`
-  width: 7.5rem;
-  height: 7.5rem;
+const PhotoDiv = styled.div<{ size: string }>`
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+	max-width; 8rem;
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -99,4 +144,24 @@ const PhotoDiv = styled.div`
     font-size: 2rem;
     color: var(--color-gray);
   }
+`;
+
+const Modal = styled.div`
+  text-align: center;
+  padding: 1rem;
+
+  & h2 {
+    font-size: 1.25rem;
+    font-weight: var(--font-semibold);
+    margin-bottom: 1rem;
+  }
+
+  & h3 {
+    color: var(--color-main);
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 1rem;
 `;
