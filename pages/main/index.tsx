20960cb5a90db816931833a9772f9bcff8f50094
SuJin <kimsujin001127@gmail.com>
2023-07-28 00:40:20 +0900

    feat: 신고 리스트 화면 완성

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -32,15 +32,20 @@ export default function Home() {
         </styles.Search>
       </styles.SearchBox>
       <styles.ListBox>
-        <styles.PicBox></styles.PicBox>
-        <styles.RightBox></styles.RightBox>
-        <styles.ListTopBox>서울특별시 동작구 노량진로 10</styles.ListTopBox>
-        <styles.ListBottomBox>
-          <styles.ListDateBox>최초 신고일: 2023.07.01 18:30</styles.ListDateBox>
-          <styles.ListTagBox>
-            <styles.Tag>미해결</styles.Tag>
-          </styles.ListTagBox>
-        </styles.ListBottomBox>
+        <styles.PicBox>
+          <Image src="/list.svg" alt="goback" width={70} height={70} priority />
+        </styles.PicBox>
+        <styles.RightBox>
+          <styles.ListTopBox>서울특별시 동작구 노량진로 10</styles.ListTopBox>
+          <styles.ListBottomBox>
+            <styles.ListDateBox>
+              최초 신고일: 2023.07.01 18:30
+            </styles.ListDateBox>
+            <styles.ListTagBox>
+              <styles.Tag>미해결</styles.Tag>
+            </styles.ListTagBox>
+          </styles.ListBottomBox>
+        </styles.RightBox>
       </styles.ListBox>
     </styles.Container>
   );
--- pages/main/main.style.ts
+++ pages/main/main.style.ts
@@ -13,7 +13,7 @@ export const Header = styled.div`
 
 export const MenuBox = styled.div`
   position: absolute;
-  top: 2.9%;
+  top: 4.4%;
 `;
 
 export const LogoBox = styled.div`
@@ -39,6 +39,7 @@ export const TopDescriptionBox = styled.div`
 
 export const SearchBox = styled.div`
   margin: 12px 0;
+  width: auto;
 `;
 
 export const Search = styled.label`
@@ -48,24 +49,50 @@ export const Search = styled.label`
     border-radius: 20px;
     background-color: #f6f6f6;
     height: 33px;
-    width: 335px;
+    width: 100%;
     border-color: #e8e8e8;
     color: #bdbdbd;
   }
 `;
 
-export const ListBox = styled.div``;
+export const ListBox = styled.div`
+  display: flex;
+  padding: 5px;
+  width: auto;
+  border-bottom: 1px solid #e8e8e8;
+`;
 
 export const PicBox = styled.div``;
 
-export const RightBox = styled.div``;
+export const RightBox = styled.div`
+  margin: 8px 0 8px 10px;
+  width: 100%;
+`;
 
-export const ListTopBox = styled.div``;
+export const ListTopBox = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+`;
 
-export const ListBottomBox = styled.div``;
+export const ListBottomBox = styled.div`
+  display: flex;
+  margin-top: 10px;
+`;
 
-export const ListDateBox = styled.div``;
+export const ListDateBox = styled.div`
+  font-size: 14px;
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+`;
 
 export const ListTagBox = styled.div``;
 
-export const Tag = styled.div``;
+export const Tag = styled.div`
+  font-size: 13px;
+  font-weight: 700;
+  background-color: #7c7c7c;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 20px;
+`;
