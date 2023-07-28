665d409d2e6af8586498826f8c67d3dfe0fadc84
SuJin <kimsujin001127@gmail.com>
2023-07-28 20:13:11 +0900

    style: 신고목록 디자인 변경

--- common/AlertList.tsx
+++ common/AlertList.tsx
@@ -5,16 +5,14 @@ const AlertList = () => {
   return (
     <styles.ListBox>
       <styles.PicBox>
-        <Image src="/list.svg" alt="goback" width={70} height={70} priority />
+        <Image src="/list.svg" alt="goback" width={68} height={68} priority />
       </styles.PicBox>
       <styles.RightBox>
+        <styles.ListTagBox>
+          <styles.Tag>미해결</styles.Tag>
+        </styles.ListTagBox>
         <styles.ListTopBox>서울특별시 동작구 노량진로 10</styles.ListTopBox>
-        <styles.ListBottomBox>
-          <styles.ListDateBox>최초 신고일: 2023.07.01 18:30</styles.ListDateBox>
-          <styles.ListTagBox>
-            <styles.Tag>미해결</styles.Tag>
-          </styles.ListTagBox>
-        </styles.ListBottomBox>
+        <styles.ListDateBox>최초 신고일 23.07.01 18:30</styles.ListDateBox>
       </styles.RightBox>
     </styles.ListBox>
   );
--- common/style/AlertList.style.tsx
+++ common/style/AlertList.style.tsx
@@ -1,23 +1,30 @@
 import styled from '@emotion/styled';
 
 export const ListBox = styled.div`
-  margin-top: 7px;
+  margin-top: 1rem;
   display: flex;
   padding: 5px 5px 7px 5px;
   width: auto;
-  border-bottom: 1px solid #e8e8e8;
+  height: 6.7rem;
+  box-shadow: 0px 2px 7px 0px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
 `;
 
-export const PicBox = styled.div``;
+export const PicBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 0.7rem;
+`;
 
 export const RightBox = styled.div`
-  margin: 8px 0 8px 10px;
-  width: 100%;
+  margin-left: 0.5rem;
+  margin: auto 0.7rem auto;
 `;
 
 export const ListTopBox = styled.div`
-  font-weight: 600;
+  font-weight: 500;
   font-size: 16px;
+  height: 2.5rem;
 `;
 
 export const ListBottomBox = styled.div`
@@ -26,19 +33,26 @@ export const ListBottomBox = styled.div`
 `;
 
 export const ListDateBox = styled.div`
-  font-size: 14px;
+  font-size: 11px;
   flex-grow: 1;
   display: flex;
   align-items: center;
 `;
 
-export const ListTagBox = styled.div``;
+export const ListTagBox = styled.div`
+  margin-bottom: 0.3rem;
+`;
 
 export const Tag = styled.div`
-  font-size: 13px;
-  font-weight: 700;
+  font-size: 11px;
+  font-weight: 600;
   background-color: #7c7c7c;
   color: white;
   padding: 5px 10px;
   border-radius: 20px;
+  height: 18px;
+  display: flex;
+  align-items: center;
+  width: 50px;
+  justify-content: center;
 `;
