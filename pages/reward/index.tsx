d09c3e9398269ace4b2a4413af7b41040fd3d996
SuJin <kimsujin001127@gmail.com>
2023-07-29 16:41:30 +0900

    refactor: 적립 내역 공통 컴포넌트로 분리

--- pages/reward/index.tsx
+++ pages/reward/index.tsx
@@ -37,15 +37,11 @@ export default function Home() {
       <styles.Container>
         <styles.BottomBox>
           <styles.DescriptionBox>적립 상세 내역</styles.DescriptionBox>
-          {/** TODO: components로 빼기 */}
-          <styles.SaveList>
-            <styles.DateBox>00.00</styles.DateBox>
-            <styles.SaveCenterBox>
-              <styles.SaveTitleBox>해결하기</styles.SaveTitleBox>
-              <styles.LocationBox>동작구 세종로 126길</styles.LocationBox>
-            </styles.SaveCenterBox>
-            <styles.SaveMoneyBox>N원</styles.SaveMoneyBox>
-          </styles.SaveList>
+          <SaveList />
+          <SaveList />
+          <SaveList />
+          <SaveList />
+          <SaveList />
         </styles.BottomBox>
       </styles.Container>
     </>
