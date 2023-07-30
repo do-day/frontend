3001112980a14667ae5daed137434384f150867a
hhkim0729 <hhkim0729@gmail.com>
2023-07-30 16:53:37 +0900

    feat: 로그인 페이지 추가
    - Button 컴포넌트에 로고 이미지, 스타일 props 추가
    - public에 로고 이미지 추가

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image';
 import styled from '@emotion/styled';
 
 interface Props {
   children: React.ReactNode;
   type?: 'button' | 'submit';
+  logoImageUrl?: string;
+  style?: React.CSSProperties;
   primary?: boolean;
   secondary?: boolean;
   rounded?: boolean;
@@ -12,6 +15,8 @@ interface Props {
 export default function Button({
   children,
   type = 'button',
+  logoImageUrl,
+  style,
   primary = true,
   secondary = false,
   rounded = false,
@@ -21,26 +26,48 @@ export default function Button({
     <StyledButton
       type={type}
       onClick={onClick}
+      style={style}
       primary={primary}
       secondary={secondary}
       rounded={rounded}
     >
-      {children}
+      {logoImageUrl && (
+        <Image
+          src={logoImageUrl}
+          alt="logo"
+          width={50}
+          height={50}
+          style={{ width: '1.5rem', height: '1.5rem' }}
+        />
+      )}
+      <ChildrenWrapper>{children}</ChildrenWrapper>
     </StyledButton>
   );
 }
 
 const StyledButton = styled.button<{
+  style?: React.CSSProperties;
   primary: boolean;
   secondary: boolean;
   rounded: boolean;
 }>`
   width: 100%;
   height: 3rem;
+  padding: 0.5rem 1rem;
+  display: flex;
+  justify-content: between;
+  align-items: center;
+  font-weight: var(--font-semibold);
   color: var(--color-white);
   background-color: ${(props) =>
     props.secondary ? 'var(--color-gray)' : 'var(--color-main)'};
   border-radius: ${(props) =>
     props.rounded ? '2rem' : 'var(--border-radius)'};
-  font-weight: var(--font-semibold);
+
+  ${(props) => props.style && String(props.style)}
+`;
+
+const ChildrenWrapper = styled.div`
+  flex: 1 1 0%;
+  line-height: 3rem;
 `;
