93cfccfb835cefb75a22a3ff643f92319ccf7cd9
SuJin <kimsujin001127@gmail.com>
2023-07-31 16:19:10 +0900

    feat: nav바 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,36 @@
 import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
-import { BiChevronLeft } from 'react-icons/bi';
+import { BiChevronLeft, BiMenu } from 'react-icons/bi';
+import { useState } from 'react';
+import Nav from './Nav';
 
 interface Props {
   title: string;
+  type?: string;
 }
 
-export default function Header({ title }: Props) {
+export default function Header({ title, type }: Props) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const HanldeOnClickBtn = () => {
+    setIsOpen(!isOpen);
+  };
+
   const router = useRouter();
 
   return (
     <StyledHeader>
-      <BackButton onClick={() => router.back()}>
-        <BiChevronLeft />
-      </BackButton>
+      {type === 'main' ? (
+        <>
+          <BackButton onClick={HanldeOnClickBtn}>
+            <BiMenu />
+          </BackButton>
+          {isOpen ? <Nav isOpen={isOpen} setIsOpen={setIsOpen} /> : ''}
+        </>
+      ) : (
+        <BackButton onClick={() => router.back()}>
+          <BiChevronLeft />
+        </BackButton>
+      )}
       <Title>{title}</Title>
     </StyledHeader>
   );
---
+++ components/Nav.tsx
@@ -0,0 +1,83 @@
+import styled from '@emotion/styled';
+
+interface NProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+const Nav = ({ isOpen, setIsOpen }: NProps) => {
+  const hanldeonClickbtn = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Backdrop onClick={hanldeonClickbtn}>
+      <FlexBox onClick={(e) => e.stopPropagation()}>
+        <MainBox>메인으로 가기</MainBox>
+        <MainBox>
+          마이페이지
+          <SubMenuBox>
+            <SubMenuNameBox>나의 신고 목록</SubMenuNameBox>
+            <SubMenuNameBox>나의 해결 목록</SubMenuNameBox>
+            <SubMenuNameBox>리워드</SubMenuNameBox>
+          </SubMenuBox>
+        </MainBox>
+        <LogOutBox>로그아웃</LogOutBox>
+      </FlexBox>
+    </Backdrop>
+  );
+};
+
+const Backdrop = styled.div`
+  background: rgba(0, 0, 0, 0.4);
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+`;
+
+const FlexBox = styled.div`
+  background-color: white;
+  position: absolute;
+  z-index: 11;
+  height: 100%;
+  width: 15rem;
+  left: 0%;
+  &.fail {
+    height: 100%;
+  }
+`;
+
+const MainBox = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  padding: 20px;
+  margin-top: 5px;
+`;
+
+const SubMenuBox = styled.div`
+  font-weight: 400;
+  margin-top: 30px;
+  margin-left: 20px;
+`;
+
+const SubMenuNameBox = styled.div`
+  margin-left: 15px;
+  margin-top: 35px;
+`;
+
+const LogOutBox = styled.div`
+  position: fixed;
+  left: 10rem;
+  bottom: 1rem;
+  color: #0083cd;
+  text-decoration: underline;
+  font-size: 16px;
+`;
+
+export default Nav;
