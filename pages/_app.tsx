4e04e1a4254630f96a9b2dc4dc48bd740d72c1d1
hhkim0729 <hhkim0729@gmail.com>
2023-08-02 00:19:08 +0900

    build: Tanstack Query Devtools 추가
    - App 컴포넌트에 Tanstack Query 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
