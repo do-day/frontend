547d564bf5c794f4d2596d5266a8b0372ba9b984
SuJin <kimsujin001127@gmail.com>
2023-08-02 15:32:40 +0900

    feat: 마이페이지 신고/해결 API 연결

--- components/ReportList.tsx
+++ components/ReportList.tsx
@@ -3,18 +3,64 @@ import { ROUTES } from '@/constants';
 import ShapedImage from '@/components/ShapedImage';
 import * as styles from '@/components/styles/ReportList.style';
 import Tag from './Tag';
+import { useEffect, useState } from 'react';
 
-const ReportList = () => {
+export interface RProps {
+  route: string;
+  reportId: number | 0;
+  solutionId: number | 0;
+  location: string;
+  photoRaincatch: string;
+  createDate: string;
+  content: string;
+  state: 'UNAPPROVAL' | 'UNRESOLVED' | 'RESOLVING' | 'RESOLVED' | 'REJECTED';
+}
+
+const ReportList = ({
+  route,
+  reportId,
+  solutionId,
+  location,
+  photoRaincatch,
+  createDate,
+  content,
+  state,
+}: RProps) => {
+  const [href, setHref] = useState<string>('');
+
+  const formateDate = (createDate: string) => {
+    const date = new Date(createDate);
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    return `${year}년 ${month}월 ${day}일 ${hours}:${minutes}`;
+  };
+
+  useEffect(() => {
+    if (route === 'admin') {
+      if (reportId === 0) setHref(ROUTES.ADMIN.SOLVE(solutionId));
+      else setHref(ROUTES.ADMIN.REPORT(reportId));
+    } else {
+      if (reportId === 0) setHref(ROUTES.SOLVES.SOLVE(solutionId));
+      else setHref(ROUTES.REPORTS.REPORT(reportId));
+    }
+  }, []);
+
   return (
     // TODO: 연결되는 주소를 reportId로 변경
     // TODO: admin일 경우에는 admin 라우트로 변경
-    <Link href={ROUTES.REPORTS.REPORT(1)}>
+    <Link href={href}>
       <styles.ListBox>
-        <ShapedImage src="/list.svg" alt="썸네일" size="5rem" />
+        {/** TODO: photoraincatch 사용한걸로 변경하기 */}
+        <ShapedImage src={'/list.svg'} alt="썸네일" size="5rem" />
         <styles.RightBox>
-          <Tag state={'UNAPPROVAL'} />
-          <styles.ListTitle>서울특별시 동작구 노량진로 10</styles.ListTitle>
-          <styles.ListDate>2023년 7월 1일 18:30</styles.ListDate>
+          <Tag state={state} />
+          <styles.ListTitle>{location}</styles.ListTitle>
+          <styles.ListDate>{formateDate(createDate)}</styles.ListDate>
         </styles.RightBox>
       </styles.ListBox>
     </Link>
--- pages/my/solves/index.tsx
+++ pages/my/solves/index.tsx
@@ -3,18 +3,42 @@ import Tab from '@/components/Tab';
 import Header from '@/components/Header';
 import Container from '@/components/Container';
 import { ROUTES } from '@/constants';
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { API_PREFIX } from '@/constants';
+axios.defaults.baseURL = API_PREFIX;
 
 export default function MySolvesHome() {
   const list = ['나의 신고 목록', '나의 해결 목록'];
   const link = [ROUTES.MY.REPORTS, ROUTES.MY.SOLVES];
+
+  const getMyReport = async (memberId: string) => {
+    return await axios.get(`/mypage/solution/${memberId}`);
+  };
+
+  const { data } = useQuery(['memberId'], () => getMyReport('1'));
+  console.log('data', data);
 
   return (
     <>
       <Header />
       <Container>
         <Tab list={list} order={1} link={link} />
-        {/** TODO: 데이터 map형태로 바꾸기 */}
-        <ReportList />
+        {data?.data.map((el: any, idx: number) => {
+          return (
+            <ReportList
+              key={el.createDate}
+              route={'my'}
+              reportId={0}
+              solutionId={el.solutionId}
+              location={el.location}
+              photoRaincatch={el.photoRaincatch}
+              createDate={el.createDate}
+              content={el.content}
+              state={el.state}
+            />
+          );
+        })}
       </Container>
     </>
   );
