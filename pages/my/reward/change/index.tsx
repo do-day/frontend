f1a53ed8585046dd9753bc297e19ab12e2e1d460
SuJin <kimsujin001127@gmail.com>
2023-08-03 00:06:09 +0900

    feat: 리워드전환 내 리워드 api 호출

--- pages/my/reward/change/index.tsx
+++ pages/my/reward/change/index.tsx
@@ -2,13 +2,21 @@ import Container from '@/components/Container';
 import Header from '@/components/Header';
 import Button from '@/components/Button';
 import * as styles from '@/pages/my/reward/change/change.style';
+import { useQuery } from '@tanstack/react-query';
+import { getTotalReward, getReward } from '@/api/reward';
+import { RewardHistory } from '@/types';
 
 export default function MyRewardChange() {
+  const { data: total } = useQuery({
+    queryKey: ['total', memberId],
+    queryFn: () => getTotalReward(Number(memberId)),
+  });
+
   return (
     <>
       <Header title="리워드 전환" hasBackButton />
       <Container>
-        <styles.TopBox>내 리워드: 0,000원</styles.TopBox>
+        <styles.TopBox>내 리워드: {total.nowReward}원</styles.TopBox>
         <styles.MoneyBox>
           <styles.MoneyInput
             placeholder="전환할 금액 입력"
@@ -18,7 +26,7 @@ export default function MyRewardChange() {
         <styles.ButtonBox>
           <styles.ChangeAllBtn>전액 입력</styles.ChangeAllBtn>
         </styles.ButtonBox>
-        <Button type="submit">전환하기</Button>
+        <Button type="submit" onClick={handleClickChange}>
       </Container>
     </>
   );
