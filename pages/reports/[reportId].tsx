40ab97b53f46d06fa14751061a95db73744643b9
hhkim0729 <hhkim0729@gmail.com>
2023-08-03 16:28:47 +0900

    feat: 신고 상세보기 지도 API 추가

--- pages/reports/[reportId].tsx
+++ pages/reports/[reportId].tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { BiCopyAlt } from 'react-icons/bi';
+import useMapView from '@/hooks/useMapView';
 import { getReport } from '@/api/report';
 import { applySolve } from '@/api/solve';
 import Container from '@/components/Container';
@@ -26,7 +27,11 @@ export default function ReportDetail() {
   const { data: report } = useQuery({
     queryKey: ['report', reportId],
     queryFn: () => getReport(Number(reportId)),
+    enabled: !!reportId,
   });
+
+  const mapRef = useRef<HTMLDivElement>(null);
+  const {} = useMapView(mapRef, report?.latitude, report?.longitude);
 
   const applySolveMutation = useMutation({
     mutationFn: () => applySolve(Number(reportId), Number(memberId)),
@@ -56,7 +61,9 @@ export default function ReportDetail() {
         <Container>
           <styles.Section>
             <styles.SectionTitle>발생 지역</styles.SectionTitle>
-            <styles.SectionDiv>지도</styles.SectionDiv>
+            <styles.SectionDiv>
+              <styles.Map ref={mapRef} />
+            </styles.SectionDiv>
             <styles.CopyButton type="button" onClick={handleClickCopy}>
               <styles.Address>{report.location}</styles.Address>
               <BiCopyAlt />
