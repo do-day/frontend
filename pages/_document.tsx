55abfe3eac4f933fe0d342288fd4ce685769d599
hhkim0729 <hhkim0729@gmail.com>
2023-08-03 15:15:00 +0900

    chore: 카카오맵 API 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Script
+          strategy="beforeInteractive"
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services&autoload=false`}
+        ></Script>
         <Main />
         <div id="modal-root"></div>
         <NextScript />
