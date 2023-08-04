3dfe9cf37fa6e669176978a7d3c4e2208a9e868b
SuJin <kimsujin001127@gmail.com>
2023-08-05 00:36:04 +0900

    feat: 회원가입 UI 완성

---
+++ pages/signup/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Button from '@/components/Button';
+import * as styles from '@/pages/signup/signup.style';
+import Input from '@/components/Input';
+
+export default function Login() {
+  return (
+    <styles.Container>
+      <styles.TitleBox>회원가입</styles.TitleBox>
+      <styles.CenterBox>
+        <styles.LoginTitleBox>휴대폰 번호(ID)</styles.LoginTitleBox>
+        <Input />
+        <styles.LoginTitleBox>비밀번호</styles.LoginTitleBox>
+        <Input />
+        <styles.ButtonBox>
+          <Button>회원가입</Button>
+        </styles.ButtonBox>
+      </styles.CenterBox>
+    </styles.Container>
+  );
+}
