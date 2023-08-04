bd16dba72e273cbe6b344c06953db7ae5d15b036
hhkim0729 <hhkim0729@gmail.com>
2023-08-04 11:47:42 +0900

    fix: 리스트의 이미지를 정사각형으로 고정

--- components/styles/ListItem.style.ts
+++ components/styles/ListItem.style.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 export const ListBox = styled.div`
+  max-height: 9rem;
   margin-bottom: 1rem;
   display: flex;
   align-items: center;
@@ -8,6 +9,27 @@ export const ListBox = styled.div`
   padding: 1rem;
   box-shadow: var(--box-shadow);
   border-radius: var(--border-radius);
+`;
+
+export const ImageBox = styled.div`
+  flex-basis: 30%;
+  position: relative;
+  max-width: 7rem;
+
+  &:after {
+    display: block;
+    content: '';
+    padding-bottom: 100%;
+    max-height: 7rem;
+  }
+
+  & > img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: var(--border-radius);
+  }
 `;
 
 export const RightBox = styled.div`
